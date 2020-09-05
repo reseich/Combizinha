e695d713ea039ebf7f4601779299b0289bf25d6b
Rafael Eich da Silva <reseich@gmail.com>
2020-09-05 12:27:45 -0400

    add react to project

--- src/repositories/implementations/Mongo/util/Utils.ts
+++ src/repositories/implementations/Mongo/util/Utils.ts
@@ -5,7 +5,7 @@ import {Recipe} from "../../../../entities/Recipe";
 
 export default class Utils {
    static seedDatabase() {
-        let recipes = JSON.parse(fs.readFileSync(__dirname+'\\recipes.txt').toString())
+        let recipes = JSON.parse(fs.readFileSync(__dirname+'/recipes.txt').toString())
         recipes.forEach((recipe:Recipe)=>{
             recipe.createdAt = Date.now()
             RecipeSchema.create(recipe).then(r => {console.log(r.name)})
